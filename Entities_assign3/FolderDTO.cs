e2c95b032a9d0fb209c496a16bd7ffc2b1a0ec4f
hsairashakeel <sairashakeel.py@gmail.com>
2020-04-14 20:56:38 -0400

    folder

---
+++ Entities_assign3/FolderDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_assign3
+{
+    public class FolderDTO
+    {
+        public int UserID { get; set; }
+        public int folderId { get; set; }
+        public int parentFolderId { get; set; }
+        public String FolderName { get; set; }
+
+    }
+}
